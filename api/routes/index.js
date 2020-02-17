b5550982ffd282fc36e9fe4df24c6f191bb29e11
krekorian@gmail.com <krekorian@gmail.com>
2020-02-17 09:08:48 -0800

    push

---
+++ api/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+const multer = require('multer')
+const upload = multer({ dest: 'public/images/uploads' })
+
+router.post('/uploadFile', upload.single('meme'), function (req, res, next) {
+  res.json(req.file);
+});
+
+router.post('/uploadFiles', upload.array('meme', 2), function (req, res, next) {
+  res.json(req.files);
+});
+
+module.exports = router;
